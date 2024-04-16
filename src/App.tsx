93a7ed4731259f06e3733b3d2da413afa8579572
Ishaan Saini <77259553+Ramleton@users.noreply.github.com>
2024-04-16 15:17:53 -0400

    Added a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
+import Header from './components/Header';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const App = () => {
   return (
     <AppContainer>
-
+      <GlobalStyles />
+      <Header></Header>
+      <OptionsGrid />
     </AppContainer>
   );
 };
 
-const AppContainer = styled.div`
+const GlobalStyles = createGlobalStyle`
+  body, h1, ul, ol, p, button, input {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const OptionsGrid = styled.div`
   display: grid;
+  flex-grow: 7;
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
 `;
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <Title>D&D Item Cards</Title>
+        </HeaderContainer>
+    );
+};
+
+const Title = styled.p`
+  color: white;
+  text-align: center;
+  font-size: 1em;
+`;
+
+const HeaderContainer = styled.h1`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #343837;
+`;
+
+export default Header;
